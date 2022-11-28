ef109ad4529828c4b40137028880b930d298861c
RyanYeaman <ryan.yeaman@icloud.com>
2022-11-28 10:10:13 -0500

    init

--- src/components/assetsComponents/NewsLetterForm.js
+++ src/components/assetsComponents/NewsLetterForm.js
@@ -2,24 +2,27 @@ import React from 'react'
 import { useState } from 'react';
 
 const NewsLetterForm = () => {
+  let email = document.querySelectorAll(".newsletter-email-input");
 
-  const [emailData, setEmailData] = useState(null)
+  let [emailData, setEmailData] = useState("")
   function getData(val) {
     setEmailData(val.target.value)
   }
-
+  
   function handleSubmit(e) {
     e.preventDefault();
-    let FormData = {
+    
+    let formData = {
       email: emailData
     }
-    console.log(FormData)
+    console.log(formData)
+    setEmailData("")
   }
   
 
   return (
     <form className='form-data' onSubmit={handleSubmit}>
-      <input className='newsletter-email-input' type='email' placeholder='email' onChange={getData}/>
+      <input className='newsletter-email-input' type='email' placeholder='email' onChange={getData} value={emailData}/>
       <button className='newsletter-submit' type='submit' >Sign Up!</button>
     </form>
   )
